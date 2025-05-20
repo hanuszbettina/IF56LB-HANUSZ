37d473c97966af6e3df0b5966f4bd3c486745ed0
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-05-20 15:59:25 +0200

    loadApi()

--- FRONTEND/PlantCare_Frontend/src/app/plant.service.ts
+++ FRONTEND/PlantCare_Frontend/src/app/plant.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Plant} from './plant';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantService {
-  
+  plants: Plant[]=[]
+  apiBaseUrl: string="https://localhost:7285/api/index.html"
+
+  constructor(private http:HttpClient) {
+    this.loadApi();
+   }
+   loadApi(): void{
+    this.http.get<Plant[]>(this.apiBaseUrl + "getDevelopers").subscribe(x => {
+      this.plants = x
+    })
+   }
 }
