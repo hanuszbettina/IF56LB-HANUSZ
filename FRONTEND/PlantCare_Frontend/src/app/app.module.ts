0b467bd641783bc74fcd1f48560bb8b80eb2d54a
hanuszbettina <hanuszbettina.sulis@gmail.com>
2025-05-20 15:53:39 +0200

    components+services

--- FRONTEND/PlantCare_Frontend/src/app/app.module.ts
+++ FRONTEND/PlantCare_Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantListComponent } from './plant-list/plant-list.component';
+import { HometipListComponent } from './hometip-list/hometip-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantListComponent,
+    HometipListComponent
   ],
   imports: [
     BrowserModule,
---
+++ FRONTEND/PlantCare_Frontend/src/app/hometip-list/hometip-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hometip-list',
+  standalone: false,
+  templateUrl: './hometip-list.component.html',
+  styleUrl: './hometip-list.component.scss'
+})
+export class HometipListComponent {
+
+}
